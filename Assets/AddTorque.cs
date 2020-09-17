380a739c44b3210830e2a29d34bc91071772f673
Mark <mark.hendriks9999@gmail.com>
2020-09-17 08:46:33 +0200

    Completed physics rad

---
+++ Assets/AddTorque.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddTorque : MonoBehaviour
+{
+
+    public float amount = 1000f;
+
+    void FixedUpdate()
+    {
+
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            GetComponent<Rigidbody>().AddTorque(transform.right * amount);
+        }
+
+    }
+}
---
+++ Assets/Gallery/Scripts/Rad/RotateRad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateRad : MonoBehaviour
+{
+    private float x;
+    private GameObject Lever;
+    private Rigidbody leverRigidBody;
+
+    void Start()
+    {
+        Lever = GameObject.Find("Lever");
+        leverRigidBody = Lever.GetComponent<Rigidbody>();
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            leverRigidBody.AddTorque(Lever.transform.forward * 100f);
+        }
+
+
+    }
+}
