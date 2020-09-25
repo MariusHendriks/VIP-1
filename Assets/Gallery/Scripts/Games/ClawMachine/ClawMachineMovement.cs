267feae1341f9fce713595b9e334eb33f29f4ccd
Mark <mark.hendriks9999@gmail.com>
2020-09-25 23:41:35 +0200

    Made a whole flipping Claw Machine wth

--- Assets/Gallery/Scripts/Games/ClawMachine/ClawMachineMovement.cs
+++ Assets/Gallery/Scripts/Games/ClawMachine/ClawMachineMovement.cs
@@ -1,21 +1,156 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 
 public class ClawMachineMovement : MonoBehaviour
 {
-    public float speed = 1f;
+    public float speedLeftAndRight = 0.2f;
+    public float speedUpAndDown = 0.2f;
+    public HingeJoint center;
+    public HingeJoint left;
+    public HingeJoint right;
+    public GameObject zPlate;
+    public GameObject xPlate;
 
+    private bool reachedBottom = false;
+    private bool grabSequenceActivated = false;
+    private bool reachedTop = false;
+    private bool hingeClosed = false;
+    private bool hingeOpened = false;
+    private float initialHeight;
+    private bool objectReleased;
+
+    void Start()
+    {
+        initialHeight = transform.localPosition.y;
+
+    }
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        if (!grabSequenceActivated)
+        {
+            transform.position += transform.right * Time.deltaTime * speedLeftAndRight * -Input.GetAxis("Vertical");
+            transform.position += transform.forward * Time.deltaTime * speedLeftAndRight * Input.GetAxis("Horizontal");
 
-        Vector3 movement = new Vector3(-moveHorizontal, 0.0f, moveVertical);
+            transform.localPosition = new Vector3(Mathf.Clamp(transform.localPosition.x, -2.5f, 2.5f), transform.localPosition.y, Mathf.Clamp(transform.localPosition.z, -2.5f, 2.5f));
 
-        GetComponent<Rigidbody>().AddForce(movement * speed);
+            zPlate.transform.localPosition = new Vector3(zPlate.transform.localPosition.x, zPlate.transform.localPosition.y, transform.localPosition.z);
+            xPlate.transform.localPosition = new Vector3(transform.localPosition.x, zPlate.transform.localPosition.y, xPlate.transform.localPosition.z);
+        }
+
+
+
+        if (grabSequenceActivated)
+        {
+            GrabSequence();
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            grabSequenceActivated = true;
+        }
+    }
+
+    private void GrabSequence()
+    {
+        if (transform.localPosition.y > 1.5f && !reachedBottom)
+        {
+            transform.position += new Vector3(0, -speedUpAndDown * Time.deltaTime, 0);
+            HandleHinges(true, -25f);
+        }
+        else
+        {
+            reachedBottom = true;
+            if (!hingeClosed && !reachedTop)
+            {
+                HandleHinges(false, 10f);
+            }
+            else
+            {
+                if (transform.localPosition.y < initialHeight && reachedBottom && !reachedTop)
+                {
+                    transform.position += new Vector3(0, speedUpAndDown * Time.deltaTime, 0);
+                }
+                else
+                {
+                    reachedTop = true;
+                    if (reachedTop)
+                    {
+                        if (transform.localPosition.x <= 2.5)
+                        {
+                            transform.position += new Vector3(speedUpAndDown * Time.deltaTime, 0, 0);
+                        }
+                        if (transform.localPosition.z <= 2.5)
+                        {
+                            transform.position += new Vector3(0, 0, speedUpAndDown * Time.deltaTime);
+                        }
+                        if (transform.localPosition.z > 2.5 && transform.localPosition.x > 2.5)
+                        {
+                            if (!hingeOpened && !objectReleased)
+                            {
+                                HandleHinges(true, -25f);
+                            }
+                            else
+                            {
+                                objectReleased = true;
+                                if (!hingeClosed)
+                                {
+
+                                    HandleHinges(false, 10f);
+                                }
+                                else
+                                {
+                                    objectReleased = false;
+                                    grabSequenceActivated = false;
+                                    reachedTop = false;
+                                    reachedBottom = false;
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void HandleHinges(bool open, float maxToGoTo)
+    {
+        var jointLimits = center.limits;
+        if (open)
+        {
+            hingeClosed = false;
+            if (jointLimits.max > maxToGoTo)
+            {
+                jointLimits.max -= 30f * Time.deltaTime;
+            }
+            else
+            {
+                hingeOpened = true;
+            }
+        }
+        else
+        {
+            hingeOpened = false;
+            if (jointLimits.max < maxToGoTo)
+            {
+                jointLimits.max += 40f * Time.deltaTime;
+
+            }
+            else
+            {
+                hingeClosed = true;
+            }
+        }
+        center.limits = jointLimits;
+        left.limits = jointLimits;
+        right.limits = jointLimits;
     }
 }
 
