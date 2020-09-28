f1c06da909e7994c679bfd91ae4f3a1a8c6646b4
Mark <mark.hendriks9999@gmail.com>
2020-09-29 01:17:07 +0200

    Added particles

--- Assets/Gallery/Scripts/Robot/RobotMovementV2.cs
+++ Assets/Gallery/Scripts/Robot/RobotMovementV2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class RobotMovementV2 : MonoBehaviour
 {
@@ -9,21 +10,34 @@ public class RobotMovementV2 : MonoBehaviour
     public float jumpForce = 8f;
     public float movementSpeed = 200f;
     public float afkTimer = 10.0f;
+    public float lowJumpMultiplier = 1.5f;
+    public float fallMultiplier = 2f;
+
+    public ParticleSystem JetpackSmokeLeft;
+    public ParticleSystem JetpackSmokeRight;
+    public ParticleSystem JetpackFireLeft;
+    public ParticleSystem JetpackFireRight;
+    public GameObject JetpackLightLeft;
+    public GameObject JetpackLightRight;
 
     private float startTime;
     private float InitialAfkTimer;
     private bool moved = false;
     private bool isDancing = false;
 
-    private bool canJump = false;
     private bool jump = false;
     private Animator animator;
+    private Rigidbody rigidBody;
+
+
 
     private void Start()
     {
+
         InitialAfkTimer = afkTimer;
         animator = GetComponent<Animator>();
         animator.enabled = true;
+        rigidBody = GetComponent<Rigidbody>();
     }
     // Update is called once per frame
     void Update()
@@ -49,12 +63,16 @@ public class RobotMovementV2 : MonoBehaviour
         }
 
 
-        if (Input.GetButtonDown("Jump") && canJump && !jump)
+        if (Input.GetButtonDown("Jump") && !jump)
         {
+            StartParticles();
             jump = true;
+            ResetPosition();
+
         }
         if (Input.GetButtonUp("Jump"))
         {
+            StopParticles();
             jump = false;
         }
 
@@ -65,6 +83,16 @@ public class RobotMovementV2 : MonoBehaviour
             isDancing = true;
             animator.SetBool("isDancing", true);
         }
+
+        if (rigidBody.velocity.y < 0)
+        {
+            rigidBody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rigidBody.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rigidBody.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
 
     }
     void ResetPosition()
@@ -77,19 +105,35 @@ public class RobotMovementV2 : MonoBehaviour
     }
     void FixedUpdate()
     {
-        if (jump && canJump)
+        if (jump)
         {
-            canJump = false;
             GetComponent<Rigidbody>().AddForce(0, jumpForce, 0, ForceMode.Impulse);
         }
+
     }
     private void OnDisable()
     {
         animator.SetBool("isWalking", false);
         ResetPosition();
     }
-    void OnCollisionEnter(Collision collision)
+    private void StopParticles()
     {
-        canJump = true;
+        JetpackSmokeLeft.Stop();
+        JetpackSmokeRight.Stop();
+        JetpackFireLeft.Stop();
+        JetpackFireRight.Stop();
+        JetpackLightLeft.GetComponent<Light>().intensity = 0;
+        JetpackLightRight.GetComponent<Light>().intensity = 0;
+    }
+    private void StartParticles()
+    {
+        JetpackSmokeLeft.Play();
+        JetpackSmokeRight.Play();
+        JetpackFireLeft.Play();
+        JetpackFireRight.Play();
+        JetpackLightLeft.GetComponent<Light>().intensity = 1;
+        JetpackLightRight.GetComponent<Light>().intensity = 1;
+
+
     }
 }
