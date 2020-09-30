299aa689e54d0b9c2f270b30ca857b3bf1fbd492
CrunchyPenguin <36261122+CrunchyPenguin@users.noreply.github.com>
2020-09-30 14:45:36 +0200

    Simple visitor walk

--- Assets/Gallery/Scripts/VisitorPathFinding.cs
+++ Assets/Gallery/Scripts/VisitorPathFinding.cs
@@ -7,11 +7,14 @@ public class VisitorPathFinding : MonoBehaviour
 {
     public GameObject toFollow;
     private NavMeshAgent agent;
+    private Animator animator;
+    private bool isWalking;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,12 +22,23 @@ public class VisitorPathFinding : MonoBehaviour
     {
         if(Vector3.Distance(toFollow.transform.position, transform.position) > 3)
         {
+            if (!isWalking)
+            {
+                animator.SetBool("isWalking", true);
+                isWalking = true;
+            }
             agent.isStopped = false;
             agent.destination = toFollow.transform.position;
         }
         else
         {
+            if (isWalking)
+            {
+                animator.SetBool("isWalking", false);
+                isWalking = false;
+            }
             agent.isStopped = true;
+            animator.SetBool("isWalking", false);
         }
     }
 }
