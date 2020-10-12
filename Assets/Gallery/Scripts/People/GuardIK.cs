99ad2b24eea1f64aa279db0f0f01c553e98e9101
Mark <mark.hendriks9999@gmail.com>
2020-10-12 14:56:17 +0200

    Please push

---
+++ Assets/Gallery/Scripts/People/GuardIK.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardIK : MonoBehaviour
+{
+    public GameObject targetObject;
+    [Range(0.0f, 1.0f)]
+    public float IKWeight = 0.8f;
+    public bool rightHand = true;
+    public bool leftHand = false;
+
+    private Animator animator;
+    private Transform rightTargetObject;
+    private Transform leftTargetObject;
+    private Vector3 rightTargetPos;
+    private Vector3 leftTargetPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rightTargetObject = targetObject.transform.Find("RightHandTarget");
+        leftTargetObject = targetObject.transform.Find("LeftHandTarget");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+
+        if (rightHand && rightTargetObject != null)
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.RightHand, IKWeight);
+            animator.SetIKPosition(AvatarIKGoal.RightHand, rightTargetObject.position);
+            animator.SetIKRotationWeight(AvatarIKGoal.RightHand, IKWeight);
+            animator.SetIKRotation(AvatarIKGoal.RightHand, rightTargetObject.rotation);
+        }
+
+        if (leftHand && leftTargetObject != null)
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, IKWeight);
+            animator.SetIKPosition(AvatarIKGoal.LeftHand, leftTargetObject.position);
+            animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, IKWeight);
+            animator.SetIKRotation(AvatarIKGoal.LeftHand, leftTargetObject.rotation);
+        }
+    }
+}
--- Assets/Gallery/Scripts/People/VisitorAI.cs
+++ Assets/Gallery/Scripts/People/VisitorAI.cs
@@ -10,15 +10,21 @@ public class VisitorAI : MonoBehaviour
     public List<GameObject> artObjects;
     public float interestedTime = 6f;
     public GameObject forceTarget;
+    public bool forceTargetEnabled = false;
+
+    //tidelijk
+    public GameObject target;
 
 
-    private GameObject target;
     private NavMeshAgent agent;
     private Animator animator;
     private bool isWalking;
     private float timer = 0f;
     private bool activeActivity = false;
     private string currentActivity;
+
+    private string[] artpieceTags = new string[] { "AIInteractable", "AIViewable" };
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,16 +33,18 @@ public class VisitorAI : MonoBehaviour
 
         target = gameObject;
         GetTarget();
-
     }
-
-
 
     // Update is called once per frame
     void Update()
     {
-
         var targetPos = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+
+        if (!target.GetComponent<ArtpieceProps>())
+        {
+            AddArtpieceProps(target);
+        }
+
         if (Vector3.Distance(targetPos, transform.position) > target.GetComponent<ArtpieceProps>().inspectionDistance)
         {
             if (!isWalking)
@@ -72,9 +80,18 @@ public class VisitorAI : MonoBehaviour
                 EndActivity();
             }
         }
+    }
 
+    private void AddArtpieceProps(GameObject target)
+    {
+        target.AddComponent<ArtpieceProps>();
+    }
+
+    private void GrabObject(GameObject target)
+    {
 
     }
+
     private void PlayActivity(string activity)
     {
         var targetPos = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
@@ -96,14 +113,26 @@ public class VisitorAI : MonoBehaviour
 
     private void GetTarget()
     {
-        if (forceTarget != null)
+        if (forceTarget != null && forceTargetEnabled)
         {
             target = forceTarget;
         }
         else
         {
             Transform artpiece = artObjects[Random.Range(0, artObjects.Count)].transform;
-            target = artpiece.GetChild(Random.Range(0, artpiece.childCount)).gameObject;
+            List<GameObject> childrenArtPieces = new List<GameObject>();
+            for (int i = 0; i < artpiece.childCount; i++)
+            {
+                Transform child = artpiece.GetChild(i);
+                if (artpieceTags.Contains(child.tag))
+                {
+                    childrenArtPieces.Add(child.gameObject);
+                }
+            }
+            if(childrenArtPieces.Count > 0)
+            {
+                target = childrenArtPieces[Random.Range(0, childrenArtPieces.Count)];
+            }
         }
 
     }
